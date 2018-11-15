63e8fc1bc78d32cd5a045562b24b30bfe5e76dae
Yaris van Thiel <35812588+yaris19@users.noreply.github.com>
2018-11-15 11:29:40 +0100

    Commit 2

--- afv1.py
+++ afv1.py
@@ -4,49 +4,60 @@
 
 
 def main():
-    bestand = "alpaca.fna"
-    headers, seqs = lees_inhoud(bestand)
-
-    zoekwoord = input("Geef een zoekwoord op: ")
-    for i in range(len(headers)):
-        if zoekwoord in headers[i]:
-            print("Header:", headers[i])
-            check_is_dna = Is_Dna(seqs[i])
-            if check_is_dna:
-                print("Sequentie is DNA")
-                knipt(seqs[i])
-            else:
-                print("Sequentie is geen DNA. Er is iets fout gegaan.")
+    try:
+        bestand = "alpaca.fasta"
+        headers, seqs = lees_inhoud(bestand)
+        zoekwoord = input("Geef een zoekwoord op: ")
+        for i in range(len(headers)):
+            if zoekwoord in headers[i]:
+                print("Header:", headers[i])
+                check_is_dna = Is_Dna(seqs[i])
+                if check_is_dna:
+                    print("Sequentie is DNA")
+                    knipt(seqs[i])
+                else:
+                    print("Sequentie is geen DNA. Er is iets fout gegaan.")
+        else:
+            print('Het zoekwoord komt niet voor in de headers')
+    except TypeError:
+        print('Het bestand heeft het verkeerde formaat, gebruik ".fasta"')
 
 
 def lees_inhoud(bestands_naam):
-    bestand = open(bestands_naam)
-    headers = []
-    seqs = []
-    seq = ""
-    for line in bestand:
-        line = line.strip()
-        if line.startswith('>'):
-            if seq != "":
-                seqs.append(seq)
-                seq = ""
-            headers.append(line)
-        else:
-            seq += line.strip()
-    seqs.append(seq)
-    return headers, seqs
+    try:
+        bestand = open(bestands_naam)
+        if bestands_naam.endswith('.fasta'):
+            headers = []
+            seqs = []
+            seq = ""
+            for line in bestand:
+                line = line.strip()
+                if line.startswith('>'):
+                    if seq != "":
+                        seqs.append(seq)
+                        seq = ""
+                    headers.append(line)
+                else:
+                    seq += line.strip()
+            seqs.append(seq)
+            return headers, seqs
+    except FileNotFoundError:
+        print('Het opgegeven bestand bestaat niet')
 
 
 def Is_Dna(seq):
-    dna = False
-    a = seq.count("A")
-    t = seq.count("T")
-    c = seq.count("C")
-    g = seq.count("G")
-    total = a+t+c+g
-    if total == len(seq):
-        dna = True
-    return dna
+    try:
+        dna = False
+        a = seq.count("A")
+        t = seq.count("T")
+        c = seq.count("C")
+        g = seq.count("G")
+        total = a+t+c+g
+        if total == len(seq):
+            dna = True
+        return dna
+    except ZeroDivisionError:
+        print('Er is gedeeld door 0 (nul), dit kan niet')
 
 
 def knipt(alpaca_seq):
